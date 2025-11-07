3d68179996e647e87238eb1bdccae80507154345
mhsn443 <majidpour443@gmail.com>
2025-11-07 17:43:02 +0330

    feat(category): replace useParams with generateStaticParams

--- app/categories/[category]/[[...page]]/page.tsx
+++ app/categories/[category]/[[...page]]/page.tsx
@@ -1,26 +1,58 @@
-"use client";
-
 import PostsListPagination from "@/components/ui/posts-list-pagination";
 import BlogPost from "@/components/ui/blog-post";
 import { Separator } from "@/components/ui/separator";
 import { postsList } from "@/data/posts-list";
-import { notFound, redirect, useParams } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { categories } from "@/data/category";
 import { featuredCategories } from "@/data/featured-category";
 
-export default function CategoryPosts() {
-  const params = useParams();
-  console.log(params);
-  const category = params.category as string;
+type StaticParams = {
+  category: string;
+  page?: string[];
+};
 
-  const allCategories = categories.concat(featuredCategories);
+const allCategories = categories.concat(featuredCategories);
+const POSTS_PER_PAGE = 8;
+
+export const generateStaticParams = () => {
+  return categories
+    .map((category) => {
+      const categoryPosts = postsList.filter(
+        (post) => post.category.slug === category.slug,
+      );
+
+      const totalPages = Math.ceil(categoryPosts.length / POSTS_PER_PAGE);
+
+      const pages: StaticParams[] = [];
+
+      for (let page = 1; page <= totalPages; page++) {
+        pages.push({
+          category: category.slug,
+          page: page === 1 ? [] : [`page/${page}`],
+        });
+      }
+
+      return pages;
+    })
+    .flat();
+};
+
+export default async function CategoryPosts({
+  params,
+}: {
+  params: StaticParams;
+}) {
+  const { category, page } = await params;
+
   const pageTitle =
     allCategories.find((item) => item.slug === category)?.name || "";
 
   // Show 404 error if the page parameter exists but is not "page"
-  if (params.page && params.page?.[0] !== "page") notFound();
+  if (page && (page?.length === 1 || page?.length > 2 || page?.[0] !== "page"))
+    notFound();
 
-  if (Number(params.page?.[1]) === 1) {
+  // Redirects URL "/page" or "/page/1" to category page
+  if (page?.[0] === "page" && page?.[1] === "1") {
     redirect(`/categories/${category}`);
   }
 
@@ -37,15 +69,19 @@ export default function CategoryPosts() {
   // Show 404 error if no posts are found or category doesn't exist
   if (!filteredPosts.length) notFound();
 
-  const page = params.page ? Number(params.page[1]) : 1;
-  const POSTS_PER_PAGE = 8;
-
   const totalPages = Math.ceil(filteredPosts.length / POSTS_PER_PAGE);
 
-  // Show 404 if the page number is invalid or out of range
-  if (isNaN(page) || page > totalPages) notFound();
+  // Show 404 error if the page number is invalid or out of range
+  if (
+    page &&
+    (isNaN(Number(page?.[1])) ||
+      Number(page?.[1]) > totalPages ||
+      Number(page?.[1]) < 1)
+  )
+    notFound();
 
-  const startIndex = (page - 1) * POSTS_PER_PAGE;
+  const pageNumber = Number(page?.[1]) || 1;
+  const startIndex = (pageNumber - 1) * POSTS_PER_PAGE;
   const currentPostsList = filteredPosts.slice(
     startIndex,
     startIndex + POSTS_PER_PAGE,
@@ -70,7 +106,7 @@ export default function CategoryPosts() {
         ))}
       </div>
       <PostsListPagination
-        currentPage={page}
+        currentPage={pageNumber}
         totalPages={totalPages}
         basePath={`/categories/${category}`}
       />
